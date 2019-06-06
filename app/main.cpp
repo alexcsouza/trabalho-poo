0db09ea106d8e65eb0cc9910242c83feccab84f2
Alex C. Souza <alexdcesouza@gmail.com>
2019-06-06 19:08:07 -0300

    Commit inicial, esqueleto...

---
+++ app/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: alex
+ *
+ * Created on 25 de Novembro de 2018, 22:23
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
